c8db50b6eec120f44162aa9df4b0a9cfec16ff44
Abdelrahman Daim <adaim@meta.com>
2025-05-08 13:55:28 +0800

    adbconnection: Reduce severity of 'cannot load the jdwp agent' logs to remove spam
    Summary: 'Not starting debugger since process cannot load the jdwp agent'
    is not an error. it just means that the launched app is not debuggable
    so ART won't start the JDWP thread. By default processes are not
    debuggable so having this log at error level is confusing and spammy.
    Making it verbose instead so it's not logged by default.
    
    Test: The said log no longer appears during boot up as various processes
    start up.
    
    Change-Id: I1844813c09f3a7d250ea6a055a5552f4db013b6b
    Signed-off-by: Abdelrahman Daim <adaim@meta.com>

--- adbconnection/adbconnection.cc
+++ adbconnection/adbconnection.cc
@@ -101,7 +101,7 @@ void AdbConnectionDebuggerController::StartDebugger() {
   if (IsDebuggingPossible() || art::Runtime::Current()->IsProfileableFromShell()) {
     connection_->StartDebuggerThreads();
   } else {
-    LOG(ERROR) << "Not starting debugger since process cannot load the jdwp agent.";
+    LOG(VERBOSE) << "Not starting debugger since process cannot load the jdwp agent.";
   }
 }
 
